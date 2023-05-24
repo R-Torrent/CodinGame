f349cefb5e1fe8f577bd69bb3ef886ada017c4fc
R-Torrent <rta@alumnes.upm.es>
2023-05-24 02:24:23 +0200

    The Descent: Completed

---
+++ Puzzles/ClassicPuzzles/Easy/TheDescent.java
@@ -0,0 +1,40 @@
+package Puzzles.ClassicPuzzles.Easy;
+
+import java.util.*;
+
+/*
+ * The Descent
+ * Puzzles > Classic Puzzle > Easy
+ */
+
+/**
+ * The while loop represents the game.
+ * Each iteration represents a turn of the game
+ * where you are given inputs (the heights of the mountains)
+ * and where you have to print an output (the index of the mountain to fire on)
+ * The inputs you are given are automatically updated according to your last actions.
+ **/
+class TheDescent {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        // game loop
+        while (true) {
+            int max = -1, highest = -1;
+            for (int i = 0; i < 8; i++) {
+                int mountainH = in.nextInt(); // represents the height of one mountain.
+                if (mountainH > max) {
+                    highest = i;
+                    max = mountainH;
+                }
+            }
+
+            // Write an action using System.out.println()
+            // To debug: System.err.println("Debug messages...");
+
+            System.out.println(highest); // The index of the mountain to fire on.
+        }
+    }
+
+}
