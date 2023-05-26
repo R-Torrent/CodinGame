80330e4ba3198cc0c46320449935717568ee0f16
R-Torrent <rta@alumnes.upm.es>
2023-05-26 17:43:41 +0200

    Horse-Racing Duals: Completed

---
+++ Puzzles/ClassicPuzzles/Easy/HorseRacingDuals.java
@@ -0,0 +1,36 @@
+package Puzzles.ClassicPuzzles.Easy;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+/*
+ * Horse-Racing Duals
+ * Puzzles > Classic Puzzle > Easy
+ */
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+public class HorseRacingDuals {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        List<Integer> strengths = new ArrayList<>(N);
+        for (int i = 0; i < N; i++) {
+            int pi = in.nextInt();
+            strengths.add(pi);
+        }
+        Collections.sort(strengths);
+        OptionalInt closest = IntStream.range(0, N - 1).map(i -> strengths.get(i + 1) - strengths.get(i)).min();
+
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        closest.ifPresent(System.out::println);
+        in.close();
+    }
+
+}
