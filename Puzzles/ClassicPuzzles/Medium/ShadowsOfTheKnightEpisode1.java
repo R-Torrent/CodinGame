aad0ff5915e2c2102090ff96190b7a06599c55ad
R-Torrent <rta@alumnes.upm.es>
2023-05-27 04:12:53 +0200

    Shadows of the Knight - Episode 1: Completed

--- Puzzles/ClassicPuzzles/Medium/ShadowsOfTheKnightEpisode1.java
+++ Puzzles/ClassicPuzzles/Medium/ShadowsOfTheKnightEpisode1.java
@@ -1,7 +1,6 @@
 package Puzzles.ClassicPuzzles.Medium;
 
 import java.util.*;
-import java.util.function.BiPredicate;
 
 /*
  * Shadows of the Knight - Episode 1
@@ -22,63 +21,84 @@ public class ShadowsOfTheKnightEpisode1 {
         int X0 = in.nextInt();
         int Y0 = in.nextInt();
 
-
-        List<Window> targets = new ArrayList<>(W * H);
-        for (int i = 0; i < W; i++)
-            for (int j = 0; j < H; j++)
-                targets.add(new Window(i, j));
-
+        Point x = new Point(X0, Y0);
+        // xtl : top-left
+        // xbr : bottom-right
+        Point xtl = new Point(0, 0);
+        Point xbr = new Point(W - 1, H - 1);
         // game loop
         while (true) {
-            final int X = X0, Y = Y0;
-            targets.remove(new Window(X, Y));
             String bombDir = in.next(); // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
             BombDir bd = BombDir.findDir(bombDir);
-            targets.removeIf(window -> !window.inHotZone(X, Y, bd));
-
-            int XcmN = 0, YcmN = 0;
-            for (Window w : targets) {
-                XcmN += w.x;
-                YcmN += w.y;
-            }
-            int minMoment = Integer.MAX_VALUE;
-            Window jumpTo = targets.get(targets.size() / 2);
-            for (Window w : targets) {
-                w.calcMoment(XcmN, YcmN, targets.size());
-                if (w.moment < minMoment) {
-                    jumpTo = w;
-                    minMoment = w.moment;
-                }
-            }
-            X0 = jumpTo.x;
-            Y0 = jumpTo.y;
+            bd.reduceSearchBox(x, xtl, xbr);
+            x.substituteWith(xtl.midwayToPoint(xbr));
 
             // Write an action using System.out.println()
             // To debug: System.err.println("Debug messages...");
 
             // the location of the next window Batman should jump to.
-            System.out.println(X0 + " " + Y0);
+            System.out.println(x.x + " " + x.y);
         }
+    }
+
+    static class Point {
+
+        int x;
+        int y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        Point midwayToPoint(Point point) {
+            return new Point((x + point.x) / 2, (y + point.y) / 2);
+        }
+
+        public void substituteWith(Point point) {
+            x = point.x;
+            y = point.y;
+        }
+
+    }
+
+    @FunctionalInterface
+    interface TriFunction<T, U, V, R> {
+
+        R apply(T t, U u, V v);
+
     }
 
     static enum BombDir {
 
-        UP        ("U",  (dx, dy) -> dx == 0 && dy  < 0),
-        UPRIGHT   ("UR", (dx, dy) -> dx  > 0 && dy  < 0),
-        RIGHT     ("R",  (dx, dy) -> dx  > 0 && dy == 0),
-        DOWNRIGHT ("DR", (dx, dy) -> dx  > 0 && dy  > 0),
-        DOWN      ("D",  (dx, dy) -> dx == 0 && dy  > 0),
-        DOWNLEFT  ("DL", (dx, dy) -> dx  < 0 && dy  > 0),
-        LEFT      ("L",  (dx, dy) -> dx  < 0 && dy == 0),
-        UPLEFT    ("UL", (dx, dy) -> dx  < 0 && dy  < 0),
-        ERROR     ("ER", (dx, dy) -> false);
+        UP        ("U",  (x0, xtl, xbr) -> new Point(x0.x, xtl.y),
+                                 (x0, xtl, xbr) -> new Point(x0.x, x0.y - 1)),
+        UPRIGHT   ("UR", (x0, xtl, xbr) -> new Point(x0.x + 1, xtl.y),
+                                 (x0, xtl, xbr) -> new Point(xbr.x, x0.y - 1)),
+        RIGHT     ("R",  (x0, xtl, xbr) -> new Point(x0.x + 1, x0.y),
+                                 (x0, xtl, xbr) -> new Point(xbr.x, x0.y)),
+        DOWNRIGHT ("DR", (x0, xtl, xbr) -> new Point(x0.x + 1, x0.y + 1),
+                                 (x0, xtl, xbr) -> xbr),
+        DOWN      ("D",  (x0, xtl, xbr) -> new Point(x0.x, x0.y + 1),
+                                 (x0, xtl, xbr) -> new Point(x0.x, xbr.y)),
+        DOWNLEFT  ("DL", (x0, xtl, xbr) -> new Point(xtl.x , x0.y + 1),
+                                 (x0, xtl, xbr) -> new Point(x0.x - 1, xbr.y)),
+        LEFT      ("L",  (x0, xtl, xbr) -> new Point(xtl.x, x0.y),
+                                 (x0, xtl, xbr) -> new Point(x0.x - 1, x0.y)),
+        UPLEFT    ("UL", (x0, xtl, xbr) -> xtl,
+                                 (x0, xtl, xbr) -> new Point(x0.x - 1, x0.y - 1)),
+        ERROR     ("ER", null, null);
 
         private final String bombDir;
-        private final BiPredicate<Integer, Integer> detected;
+        private final TriFunction<Point, Point, Point, Point> fxtl;
+        private final TriFunction<Point, Point, Point, Point> fxbr;
 
-        BombDir(String bombDir, BiPredicate<Integer, Integer> detected) {
+        BombDir(String bombDir,
+                TriFunction<Point, Point, Point, Point> fxtl,
+                TriFunction<Point, Point, Point, Point> fxbr) {
             this.bombDir = bombDir;
-            this.detected = detected;
+            this.fxtl = fxtl;
+            this.fxbr = fxbr;
         }
 
         static BombDir findDir(String bombDir) {
@@ -88,38 +108,9 @@ public class ShadowsOfTheKnightEpisode1 {
             return BombDir.ERROR;
         }
 
-        boolean inHotZone(int dx, int dy) {
-            return detected.test(dx, dy);
-        }
-
-    }
-
-    static class Window {
-
-        final int x;
-        final int y;
-        int moment;
-
-        Window(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        boolean inHotZone(int x0, int y0, BombDir bd) {
-            return bd.inHotZone(x - x0, y - y0);
-        }
-
-        // "Almost" |Moment(x,y)|
-        void calcMoment(int xcmN, int ycmN, int N) {
-            moment = Math.abs(x * N - xcmN) + Math.abs(y * N - ycmN);
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Window window = (Window) o;
-            return x == window.x && y == window.y;
+        void reduceSearchBox(Point x, Point xtl, Point xbr) {
+            xtl.substituteWith(fxtl.apply(x, xtl, xbr));
+            xbr.substituteWith(fxbr.apply(x, xtl, xbr));
         }
 
     }
