9a9c7751a066c30d9aa1a3bae5e544081def911e
RogerTorrent <rta@alumnos.upm.es>
2024-04-30 00:59:05 +0200

    Recurring Decimals: Completed

--- Puzzles/ClassicPuzzle/Expert/Recurring_Decimals.c
+++ Puzzles/ClassicPuzzle/Expert/Recurring_Decimals.c
@@ -1,7 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
 
 /*
  * Recurring Decimals
@@ -26,16 +24,33 @@ int main()
  *
  * 10·rem[i] = dig[i]·divisor + rem[i+1]
  *
- * frcds will store the number of fractional digits
- * repds will store the number of repeating digits
+ * fract_digits will store the number of fractional digits
+ * rep_digits will store the number of repeating digits
  */
 	fract_t *fract0 = malloc((N - 1) * sizeof(fract_t)), *fract;
-	int frcds = 0, repds = 0;
+	int fract_digits, rep_digits, i;
 	div_t op;
-	for(op = div(1, N), fract = fract0; op.rem; fract++) {
+	for(op = div(1, N), fract = fract0, fract_digits = 0, rep_digits = 0;
+			op.rem && !rep_digits; fract++, fract_digits++) {
 		fract->rem = op.rem;
 		op = div(10 * fract->rem, N);
+		fract->dig = op.quot + '0';
+
+		i = 0;
+		do // check for remainder repetition
+			if ((fract - i)->rem == op.rem)
+				rep_digits = i + 1;
+		while (!rep_digits && i++ <= fract_digits);
 	}
+
+	printf("0.");
+	for (i = 0, fract = fract0; i < fract_digits; i++, fract++) {
+		if (rep_digits && (i == fract_digits - rep_digits))
+			putchar('(');
+		putchar(fract->dig);
+	}
+	if (rep_digits)
+		putchar(')');
 
 	free(fract0);
 	return 0;
