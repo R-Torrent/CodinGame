d3fc431ff45f4ad8bff62d78c58ad85a1b589e6f
RogerTorrent <rta@alumnos.upm.es>
2024-04-29 14:31:29 +0200

    Next puzzle to solve

---
+++ Puzzles/ClassicPuzzle/Hard/Blunder_Episode_3.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+/*
+ * Blunder - Episode 3
+ * Puzzles > Classic Puzzle > Hard
+ */
+
+char *answer[] = { "O(1)", "O(log n)", "O(n)", "O(n log n)",
+	"O(n^2)", "O(n^2 log n)", "O(n^3)", "O(2^n)" };
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	int data[999][2];
+	for (int i = 0; i < N; i++)
+		scanf("%d%d", &data[i][0], &data[i][1];
+
+// Write an answer using printf(). DON'T FORGET THE TRAILING \n
+// To debug: fprintf(stderr, "Debug messages...\n");
+
+	printf("answer\n");
+
+	return 0;
+}
