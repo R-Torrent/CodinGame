278d731b66b4f5a48919cd8597b93c52009e98f5
RogerTorrent <rta@alumnos.upm.es>
2024-05-17 20:23:09 +0200

    Started work on 'The_Total_Is_Right'

---
+++ Puzzles/ClassicPuzzle/Hard/The_Total_Is_Right-Weird_Edition.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main()
+{
+	int N, a;
+	scanf("%d%d", &N, &a);
+
+	// Write an answer using printf(). DON'T FORGET THE TRAILING \n
+	// To debug: fprintf(stderr, "Debug messages...\n");
+
+	printf("answer\n");
+
+	return 0;
+}
