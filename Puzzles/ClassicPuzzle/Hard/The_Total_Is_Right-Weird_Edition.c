3d24b032abcd6b41c8aca64aae0ca3f5b28bcdd8
RogerTorrent <rta@alumnos.upm.es>
2024-05-18 14:15:11 +0200

    Still timeouts with solutions > 8...

--- Puzzles/ClassicPuzzle/Hard/The_Total_Is_Right-Weird_Edition.c
+++ Puzzles/ClassicPuzzle/Hard/The_Total_Is_Right-Weird_Edition.c
@@ -24,52 +24,50 @@ typedef struct {
 } stack_t;
 
 bool is_valid(char *, int);
-bool evaluates_to_N(stack_t *, int, int, char *, int);
+void evaluates_to_N(int *, int, stack_t *,char *, int);
 void duplicate_stack(stack_t *, stack_t *);
 void push(stack_t *, int);
 int pop(stack_t *);
 
 int compare(const void *, const void *);
 
+int N, a;
+
 int main()
 {
-	int N, a;
 	scanf("%d%d", &N, &a);
 
 	char expr[2 * LIMIT + 1];
-	int sol;
-	bool found = false;
-	for (sol = 1; sol <= LIMIT && !found; sol++) {
-		int size = 2 * sol + 1;
-		expr[0] = expr[1] = 1; // 1 == 'a' in the above explanation
-		expr[size - 1] = 0;    // 0 == 'o' in the above explanation
-		// starting expr = a a (a a a ... a o ... o o o) o
-		for (int i = 2; i < size - 1; i++)
-			expr[i] = i <= sol;
+	int sol = LIMIT + 1;
 
-		while (true) {
-			stack_t st = { .sp = 0 };
-			if (is_valid(expr, size) && evaluates_to_N(&st, N, a, expr, size)) {
-				found = true;
+	int size = 2 * LIMIT + 1;
+	expr[0] = expr[1] = 1; // 1 == 'a' in the above explanation
+	expr[size - 1] = 0;    // 0 == 'o' in the above explanation
+	// starting expr = a a (a a a ... a o ... o o o) o
+	for (int i = 2; i < size - 1; i++)
+		expr[i] = i <= LIMIT;
+
+	while (true) {
+		stack_t st = { .sp = 0 };
+		if (is_valid(expr, size))
+			evaluates_to_N(&sol, 0, &st, expr, size);
+		// permutations with duplications for the central ( a o ... ) portion
+		// find rightmost one with a zero to its right
+		int i;
+		for (i = size - 3; i >= 2; i--)
+			if (expr[i] && !expr[i + 1])
 				break;
-			}
-			// permutations with duplications for the central ( a o ... ) portion
-			// find rightmost one with a zero to its right
-			int i;
-			for (i = size - 3; i >= 2; i--)
-				if (expr[i] && !expr[i + 1])
-					break;
-			if (i < 2) // none found
-				break;
-			else {
-				expr[i] = 0; // swap elements
-				expr[i + 1] = 1;
-				// sort in descending order
-				qsort(expr + i + 1, size - i - 2, 1, compare);
-			}
+		if (i < 2) // none found
+			break;
+		else {
+			expr[i] = 0; // swap elements
+			expr[i + 1] = 1;
+			// sort in descending order
+			qsort(expr + i + 1, size - i - 2, 1, compare);
 		}
 	}
-	if (!found) {
+
+	if (sol > LIMIT) {
 		printf("Solution not found\n");
 		return 1;
 	}
@@ -94,35 +92,45 @@ bool is_valid(char *expr, int size)
 	return true;
 }
 
-bool evaluates_to_N(stack_t *st, int N, int a, char *expr, int size)
+void evaluates_to_N(int *sol, int pushes, stack_t *st, char *expr, int size)
 {
-	if (!size--)
-		return pop(st) == N;
+	if (!size-- || pushes >= *sol) // cut branch
+		return;
+	if (st->sp) {
+		int op = pop(st);
+		if (op == N) {
+			*sol = pushes;
+			return;
+		}
+		push(st, op);
+	}
+
 	if (*expr++) { // push value 'a'
 		push(st, a);
-		return evaluates_to_N(st, N, a, expr, size);
+		evaluates_to_N(sol, ++pushes, st, expr, size);
+		return;
 	}
+
 	int op2 = pop(st), op1 = pop(st);
 	stack_t st_copy;
+
 	duplicate_stack(&st_copy, st);
 	push(&st_copy, op1 + op2); // add
-	if (evaluates_to_N(&st_copy, N, a, expr, size))
-		return true;
+	evaluates_to_N(sol, pushes, &st_copy, expr, size);
+
 	duplicate_stack(&st_copy, st);
 	push(&st_copy, op1 - op2); // subtract
-	if (evaluates_to_N(&st_copy, N, a, expr, size))
-		return true;
+	evaluates_to_N(sol, pushes, &st_copy, expr, size);
+
 	duplicate_stack(&st_copy, st);
 	push(&st_copy, op1 * op2); // multiply
-	if (evaluates_to_N(&st_copy, N, a, expr, size))
-		return true;
+	evaluates_to_N(sol, pushes, &st_copy, expr, size);
+
 	if (op2 && !(op1 % op2)) {
 		duplicate_stack(&st_copy, st);
 		push(&st_copy, op1 / op2); // divide
-		if (evaluates_to_N(&st_copy, N, a, expr, size))
-			return true;
+		evaluates_to_N(sol, pushes, &st_copy, expr, size);
 	}
-	return false;
 }
 
 void duplicate_stack(stack_t *dst, stack_t *src)
@@ -134,16 +142,10 @@ void duplicate_stack(stack_t *dst, stack_t *src)
 
 void push(stack_t *st, int i)
 {
-	if (st->sp < DEPTH)
-		st->stack[st->sp++] = i;
-	else
-		exit(2);
+	st->stack[st->sp++] = i;
 }
 
 int pop(stack_t *st)
 {
-	if (st->sp > 0)
-		return st->stack[--st->sp];
-	else
-		exit(2);
+	return st->stack[--st->sp];
 }
