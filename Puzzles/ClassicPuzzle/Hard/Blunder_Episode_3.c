ae02f8b43a6f78e729925b86ec95ea4fe562a1ea
RogerTorrent <rta@alumnos.upm.es>
2024-05-24 21:36:13 +0200

    First steps with the linear regression

--- Puzzles/ClassicPuzzle/Hard/Blunder_Episode_3.c
+++ Puzzles/ClassicPuzzle/Hard/Blunder_Episode_3.c
@@ -1,7 +1,5 @@
-#include <stdlib.h>
+#include <math.h>
 #include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
 
 /*
  * Blunder - Episode 3
@@ -11,16 +9,33 @@
 char *answer[] = { "O(1)", "O(log n)", "O(n)", "O(n log n)",
 	"O(n^2)", "O(n^2 log n)", "O(n^3)", "O(2^n)" };
 
+// Solved with linear regression, the parameters calculated through
+// ordinary least squares (OLS).
+
 int main()
 {
 	int N;
 	scanf("%d", &N);
-	int data[999][2];
-	for (int i = 0; i < N; i++)
-		scanf("%d%d", &data[i][0], &data[i][1];
+	float y[999], x[999][8], xt[8][999], xt_x[8][8], inv_xt_x[8][8];
+	for (int i = 0; i < N; i++) {
+		float n;
+		scanf("%f%f", &y[i], &n);
+		xt[0][i] = x[i][0] = 1.0;               // 1
+		xt[1][i] = x[i][1] = logf(n);           // log(n)
+		xt[2][i] = x[i][2] = n;                 // n
+		xt[3][i] = x[i][3] = x[i][1] * x[i][2]; // n * log(n)
+		xt[4][i] = x[i][4] = x[i][2] * x[i][2]; // n ^ 2
+		xt[5][i] = x[i][5] = x[i][1] * x[i][4]; // n ^ 2 * log(n)
+		xt[6][i] = x[i][6] = x[i][2] * x[i][4]; // n ^ 3
+		xt[7][i] = x[i][7] = powf(2.0F, n);     // 2 ^ n
+	}
 
-// Write an answer using printf(). DON'T FORGET THE TRAILING \n
-// To debug: fprintf(stderr, "Debug messages...\n");
+	for (int i = 0; i < 8; i++)
+		for (int j = 0; j < 8; j++) {
+			xt_x[i][j] = 0;
+			for (int k = 0; k < 999; k++)
+				xt_x[i][j] += xt[i][k] * x[k][j];
+		}
 
 	printf("answer\n");
 
