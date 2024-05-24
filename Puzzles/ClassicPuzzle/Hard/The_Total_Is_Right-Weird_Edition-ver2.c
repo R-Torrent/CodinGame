11547b49569d9d5339a6263aa1e85db6fd2b877c
RogerTorrent <rta@alumnos.upm.es>
2024-05-24 14:45:49 +0200

    Improved performance by combining sets and lists

--- Puzzles/ClassicPuzzle/Hard/The_Total_Is_Right-Weird_Edition-ver2.c
+++ Puzzles/ClassicPuzzle/Hard/The_Total_Is_Right-Weird_Edition-ver2.c
@@ -12,22 +12,14 @@
 #define LIMIT 12 // game's limit on the appearances of number 'a'
 
 typedef struct Node {
-	struct Node *left, *right;
+	struct Node *left, *right; // left and right children in the search tree set
+	struct Node *next; // next integer with equal solution
 	int i;
 } node_t;
 
-bool check_n_insert(node_t **, int);
+bool check_op(node_t **, node_t **, int);
+node_t *check_n_insert(node_t **, int);
 void clear_tree_set(node_t *);
-
-typedef struct List {
-	struct List *next;
-	int i;
-} list_t;
-
-void add_list(list_t **, int);
-void clear_list(list_t *);
-
-bool check_op(node_t **, list_t **, int);
 
 int N, a, top;
 
@@ -36,59 +28,58 @@ int main()
 	scanf("%d%d", &N, &a);
 	top = N * a * a * a;
 
-	node_t *tree_set = NULL;
-	list_t *solutions[LIMIT] = { NULL };
+	node_t *tree_set = NULL; // ordered tree for all integers encountered
+	node_t *solutions[LIMIT] = { NULL }; // list of equal solutions
 
-	check_n_insert(&tree_set, a);
-	add_list(&solutions[0], a);
-
-	int i = 1;
-	if (a == N)
+	int u = 1; // u: uses of 'a'
+	if (check_op(&tree_set, solutions, a))
 		goto end;
-	for (++i; i <= LIMIT; i++)
-		for (int j = 1; j <= i / 2; j++) {
-			int k = i - j;
-			for (list_t *x = solutions[j - 1]; x; x = x->next)
-				for (list_t *y = solutions[k - 1]; y; y = y->next) {
-					list_t **target = solutions + i - 1;
-					if (check_op(&tree_set, target, x->i + y->i))
+	for (u = 2; u <= LIMIT; u++)
+		for (int v = 1; v <= u / 2; v++) {
+			int w = u - v; // run all partitions v + w = u
+			for (node_t *x = solutions[v - 1]; x; x = x->next)
+				for (node_t *y = solutions[w - 1]; y; y = y->next) {
+					node_t **target_list = solutions + u - 1;
+					if (check_op(&tree_set, target_list, x->i + y->i))
 						goto end;
 					int sub = x->i - y->i;
 					if (sub < 0)
 						sub = -sub;
-					if (check_op(&tree_set, target, sub))
+					if (check_op(&tree_set, target_list, sub))
 						goto end;
-					if (check_op(&tree_set, target, x->i * y->i))
+					if (check_op(&tree_set, target_list, x->i * y->i))
 						goto end;
 					if (y->i && !(x->i % y->i) &&
-						check_op(&tree_set, target, x->i / y->i))
+						check_op(&tree_set, target_list, x->i / y->i))
 							goto end;
 					if (x->i && !(y->i % x->i) &&
-						check_op(&tree_set, target, y->i / x->i))
+						check_op(&tree_set, target_list, y->i / x->i))
 							goto end;
 				}
 		}
 end:
 	clear_tree_set(tree_set);
-	for (int i = 0; i < LIMIT; i++)
-		clear_list(solutions[i]);
 
-	if (i > LIMIT) {
+	if (u > LIMIT) {
 		printf("Solution not found\n");
 		return 1;
 	}
-	printf("%d\n", i);
+	printf("%d\n", u);
 	return 0;
 }
 
-bool check_op(node_t **pnode, list_t **plist, int i)
+bool check_op(node_t **pnode, node_t **plist, int i)
 {
-	if (i < top && check_n_insert(pnode, i))
-		add_list(plist, i);
+	node_t *node;
+
+	if (i < top && (node = check_n_insert(pnode, i))) {
+		node->next = *plist;
+		*plist = node;
+	}
 	return i == N;
 }
 
-bool check_n_insert(node_t **pnode, int i)
+node_t *check_n_insert(node_t **pnode, int i)
 {
 	node_t *node = *pnode;
 
@@ -97,14 +88,14 @@ bool check_n_insert(node_t **pnode, int i)
 		node->left = node->right = NULL;
 		node->i = i;
 		*pnode = node;
-		return true;
+		return node;
 	}
 	else if (i < node->i)
 		return check_n_insert(&node->left, i);
 	else if (i > node->i)
 		return check_n_insert(&node->right, i);
 	else
-		return false;
+		return NULL;
 }
 
 void clear_tree_set(node_t *node)
@@ -113,23 +104,5 @@ void clear_tree_set(node_t *node)
 		clear_tree_set(node->left);
 		clear_tree_set(node->right);
 		free(node);
-	}
-}
-
-void add_list(list_t **plink, int i)
-{
-	list_t *link;
-
-	link = malloc(sizeof(list_t));
-	link->next = *plink;
-	link->i = i;
-	*plink = link;
-}
-
-void clear_list(list_t *link)
-{
-	if (link) {
-		clear_list(link->next);
-		free(link);
 	}
 }
