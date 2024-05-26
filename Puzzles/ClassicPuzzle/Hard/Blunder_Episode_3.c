b4d8fbd12e168bd3fe1b4b330938d83e86cfc7ad
RogerTorrent <rta@alumnos.upm.es>
2024-05-26 13:47:32 +0200

    Changed real types float -> double

--- Puzzles/ClassicPuzzle/Hard/Blunder_Episode_3.c
+++ Puzzles/ClassicPuzzle/Hard/Blunder_Episode_3.c
@@ -12,43 +12,49 @@ char *answer[] = { "O(1)", "O(log n)", "O(n)", "O(n log n)",
 // Solved with linear regression, the parameters calculated through
 // ordinary least squares (OLS).
 
-float determinant(int, float [][8]);
-float minor(int, float [][8], int, int);
+double determinant(int, double [][8]);
+double minor(int, double [][8], int, int);
 
 int main()
 {
 	int N;
 	scanf("%d", &N);
 
-	float y[999], x[999][8], xt[8][999], xt_x[8][8], xt_y[8], b[8];
+	double y[999], x[999][8], xt[8][999], xt_x[8][8], xt_y[8], b[8];
+	int found_inf = 0, p;
 	for (int i = 0; i < N; i++) {
-		float n;
-		scanf("%f%f", &n, &y[i]);
-		xt[0][i] = x[i][0] = 1.0F;              // 1
-		xt[1][i] = x[i][1] = logf(n);           // log(n)
+		double n;
+		scanf("%lf%lf", &n, &y[i]);
+		xt[0][i] = x[i][0] = 1.0;               // 1
+		xt[1][i] = x[i][1] = log(n);            // log(n)
 		xt[2][i] = x[i][2] = n;                 // n
 		xt[3][i] = x[i][3] = x[i][1] * x[i][2]; // n * log(n)
 		xt[4][i] = x[i][4] = x[i][2] * x[i][2]; // n ^ 2
 		xt[5][i] = x[i][5] = x[i][1] * x[i][4]; // n ^ 2 * log(n)
 		xt[6][i] = x[i][6] = x[i][2] * x[i][4]; // n ^ 3
-		xt[7][i] = x[i][7] = powf(2.0F, n);     // 2 ^ n
+		if (!found_inf) {
+			xt[7][i] = x[i][7] = pow(2.0, n);   // 2 ^ n
+			if (isinf(x[i][7]))
+				found_inf = 1;
+		}
 	}
+	p = found_inf ? 7 : 8;
 
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < 8; j++) {
-			xt_x[i][j] = 0.0F;
+	for (int i = 0; i < p; i++) {
+		for (int j = 0; j < p; j++) {
+			xt_x[i][j] = 0.0;
 			for (int k = 0; k < N; k++)
 				xt_x[i][j] += xt[i][k] * x[k][j];
 		}
-		xt_y[i] = 0.0F;
+		xt_y[i] = 0.0;
 		for (int j = 0; j < N; j++)
 			xt_y[i] += xt[i][j] * y[j];
 	}
-	float det_xt_x = determinant(8, xt_x);
-	for (int i = 0; i < 8; i++) {
-		b[i] = 0.0F;
-		for (int j = 0; j < 8; j++)
-			b[i] += ((i + j) % 2 ? -1 : 1) * minor(8, xt_x, j, i)
+	double det_xt_x = determinant(p, xt_x);
+	for (int i = 0; i < p; i++) {
+		b[i] = 0.0;
+		for (int j = 0; j < p; j++)
+			b[i] += ((i + j) % 2 ? -1 : 1) * minor(p, xt_x, j, i)
 					* xt_y[j] / det_xt_x;
 	}
 
@@ -58,9 +64,9 @@ int main()
 }
 
 // Laplace expansion
-float determinant(int n, float matrix[][8])
+double determinant(int n, double matrix[][8])
 {
-	float d = 0.0F;
+	double d = 0.0;
 
 	for (int j = 0; j < n; j++)
 		d += (j % 2 ? -1 : 1) * matrix[0][j] * minor(n, matrix, 0, j);
@@ -68,25 +74,24 @@ float determinant(int n, float matrix[][8])
 	return d;
 }
 
-float minor(int n, float matrix[][8], int i, int j)
+double minor(int n, double matrix[][8], int i, int j)
 {
-	float m[8][8];
+	double m[8][8];
 
-	if (n > 1) {
-		for (int k = 0, km = 0; k < n; k++) {
-			if (k == i)
+	if (n == 1)
+		return 1.0;
+
+	for (int k = 0, km = 0; k < n; k++) {
+		if (k == i)
+			continue;
+		for (int l = 0, lm = 0; l < n; l++) {
+			if (l == j)
 				continue;
-			for (int l = 0, lm = 0; l < n; l++) {
-				if (l == j)
-					continue;
-				m[km][lm] = matrix[k][l];
-				lm++;
-			}
-			km++;
+			m[km][lm] = matrix[k][l];
+			lm++;
 		}
-
-		return determinant(n - 1, m);
+		km++;
 	}
 
-	return 1.0F;
+	return determinant(n - 1, m);
 }
