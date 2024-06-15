918b198168340b1fb2ac43004af0e721d4ae3f20
R-Torrent <rta@alumnos.upm.es>
2024-06-15 19:38:06 +0200

    First version tested since Wood 2, with all four mini-games

--- Challenge/SummerChallenge2024/Silver/player.c
+++ Challenge/SummerChallenge2024/Silver/player.c
@@ -99,7 +99,7 @@ int main()
 			mgames[mg](registers + mg, indiv + mg);
 			fprintf(stderr, "Mini-game: %s\n", played[mg]); 
 			for (op = 0; op < NUMBER_OPS; op++)
-				fprintf(stderr, "  %s : %+03d\n", output[op], indiv[mg][op]);
+				fprintf(stderr, "  %s : %+03hd\n", output[op], indiv[mg][op]);
 		}
 		array_scores *sc, tally = { 0 }; // combined scores
 		for (sc = indiv; sc - indiv < nb_games; sc++)
@@ -107,7 +107,7 @@ int main()
 				tally[op] += (*sc)[op];
 		fprintf(stderr, "TOTAL SCORE:\n"); 
 		for (op = 0; op < NUMBER_OPS; op++)
-			fprintf(stderr, "  %s : %+03d\n", output[op], tally[op]);
+			fprintf(stderr, "  %s : %+03hd\n", output[op], tally[op]);
 		enum ops max_op;
 		short max_score = 0;
 		for (op = 0; op < NUMBER_OPS; op++)
@@ -136,8 +136,8 @@ void hurdles(register_t *reg, array_scores *sc)
 		return;
 	}
 
-	int pos = reg->reg[player_idx]; // current position
-	int aim;                        // position runner aims at
+	short pos = reg->reg[player_idx]; // current position
+	short aim;                        // position runner aims at
 	for (aim = pos + 1; aim < TRACK_LEN && reg->gpu[aim] == '.'; aim++)
 		;
 	switch (aim - pos) { // finish line = TRACK_LEN - 1
@@ -171,6 +171,7 @@ void hurdles(register_t *reg, array_scores *sc)
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #define ABS(a) ((a) < 0 ? (-a) : (a))
+#define SIGN(a) ((a) < 0 ? -1 : (a) > 0 ? 1 : 0)
 
 short root2(short S, short x0)
 {
@@ -214,6 +215,40 @@ void archery(register_t *reg, array_scores *sc)
 // Roller Speed Skating
 void skating(register_t *reg, array_scores *sc)
 {
+	// reset turn or stunned skater
+	if (!strcmp(reg->gpu, GAMEOVER) || reg->reg[player_idx + 3] < 0) {
+		(*sc)[UP] = 0;
+		(*sc)[DOWN] = 0;
+		(*sc)[LEFT] = 0;
+		(*sc)[RIGHT] = 0;
+		return;
+	}
+
+	short travelled[3] = {
+		reg->reg[player_idx],           // me
+		reg->reg[(player_idx + 1) % 3], // opponent 1
+		reg->reg[(player_idx + 2) % 3]  // opponent 2
+	};
+	short advantage = SIGN(travelled[0] - MAX(travelled[1], travelled[2]));
+	short risk = reg->reg[player_idx + 3];
+	short risk_order; // stores the index in gpu of each operator
+	for (enum ops op = 0; op < NUMBER_OPS; op++) {
+		risk_order = strchr(reg->gpu, initials[op]) - reg->gpu;
+		switch (risk_order) {
+		case 0: // player +1 risk -1
+			(*sc)[op] = 5 + risk;
+			break;
+		case 1: // player +2
+			(*sc)[op] = 10;
+			break;
+		case 2: // player +2 risk +1
+			(*sc)[op] = 8 - (risk << 1) - advantage;
+			break;
+		case 3: // player +3 risk +2
+			(*sc)[op] = 5 - (risk << 1) - advantage;
+			break;
+		}
+	}
 }
 
 // Diving
