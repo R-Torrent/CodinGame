6fdbe20c7e36be457f5548274133d152976ab871
RogerTorrent <rta@alumnos.upm.es>
2024-06-21 20:34:24 +0200

    Baby steps...

--- Puzzles/ClassicPuzzle/Expert/Shadows_of_the_Knight_Episode_2.c
+++ Puzzles/ClassicPuzzle/Expert/Shadows_of_the_Knight_Episode_2.c
@@ -1,7 +1,4 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
 
 /*
  * Shadows of the Knight - Episode 2
@@ -19,7 +16,7 @@ typedef struct {
 	int h;
 } rectangle_t;
 
-enum jump { V, H }; // Vertical or horizontal
+enum jump { V, H }; // vertical or horizontal
 
 int main()
 {
@@ -31,45 +28,65 @@ int main()
 	int N;
 	scanf("%d", &N);
 
-	point_t last, jump;
+	point_t jump, last;
+	enum jump dir;
+	point_t cm;
+
 	scanf("%d%d", &last.x, &last.y);
 
 	// game loop
 	while (1) {
-		// Current distance to the bomb compared to previous distance (COLDER, WARMER, SAME or UNKNOWN)
+		// current distance to the bomb compared to previous distance (COLDER, WARMER, SAME or UNKNOWN)
 		char bomb_dir[11];
 		scanf("%s", bomb_dir);
 
-		int gap = dir == V ? jump.y - last.y : jump.x - last.x;
-		int cut = (dir == V ? last.y : last.x) + gap / 2 + gap % 2;
-		if (*bomb_dir != 'U')
-			if (*bomb_dir != 'S') {
-				if (dir == V)
-					rect.h = (gap > 0 ? 1 : -1) * (cut - rect.p0.y);
-				else if (dir == H)
-					rect.w = (gap > 0 ? 1 : -1) * (cut - rect.p0.x);
-				if ((gap > 0 && *bomb_dir == 'W')) || (gap < 0 && *bomb_dir == 'C')))
-					if (dir == V)
-						rect.p0.y = cut + 1;
-					else
-						rect.p0.x = cut + 1;
-				if (*bomb_dir == 'W')
-					last = jump;
+		switch (*bomb_dir) {
+		case 'W': // WARMER
+			if (dir == V)
+				rect.h /= 2;
+			else
+				rect.w /= 2;
+			if (dir == V && jump.y > cm.y)
+				rect.p0.y = cm.y + 1;
+			else if (dir == H && jump.x > cm.x)
+				rect.p0.x = cm.x + 1;
+			break;
+		case 'C': // COLDER
+			if (dir == V)
+				rect.h /= 2;
+			else
+				rect.w /= 2;
+			if (dir == V && jump.y < cm.y)
+				rect.p0.y = cm.y + 1;
+			else if (dir == H && jump.x < cm.x)
+				rect.p0.x = cm.x + 1;
+			break;
+		case 'S': // SAME
+			if (dir == V) {
+				rect.h = 1;
+				rect.p0.y = cm.y;
 			}
 			else {
-				if (dir == V) {
-					rect.h = 1;
-					rect.p0.y = cut;
-				}
-				else {
-					rect.w = 1;
-					rect.p0.x = cut;
-				}
-			last =
+				rect.w = 1;
+				rect.p0.x = cm.x;
+			}
+			break;
+		case 'U': // UNKNOWN
+			break;
+		}
 
+		// jump favors odd-sized directions first (for a lucky chance of a 'SAME' result) and shorter sides
+		dir = rect.w == 1 || (rect.h % 2 && rect.h > 1 && !(rect.w % 2 && rect.h > rect.w)) ||
+				(!(rect.h % 2) && !(rect.w % 2) && rect.h <= rect.w) ? V : H;
 
-			printf("0 0\n");
+		// center of mass
+		point_t cm = { rect.p0.x + rect.w / 2, rect.p0.y + rect.h / 2 };
+
+		jump.x = dir == V ? last.x : 2 * cm.x - last.x - (!(rect.w % 2) ? 1 : 0);
+		jump.y = dir == V ? 2 * cm.y - last.y - (!(rect.h % 2) ? 1 : 0) : last.y;
+
+		printf("%d %d\n", jump.x, jump.y);
+
+		last = jump;
 	}
-
-	return 0;
 }
