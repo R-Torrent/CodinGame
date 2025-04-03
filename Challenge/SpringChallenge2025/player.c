70b3addca675e9f70436c01044a3cee68346514a
Roger Torrent <rtorrent@student.codam.nl>
2025-04-03 19:12:27 +0200

    Default code

---
+++ Challenge/SpringChallenge2025/player.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+int main()
+{
+	int depth;
+	scanf("%d", &depth);
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			int value;
+			scanf("%d", &value);
+		}
+	}
+
+	// Write an action using printf(). DON'T FORGET THE TRAILING \n
+	// To debug: fprintf(stderr, "Debug messages...\n");
+
+	printf("0\n");
+
+	return 0;
+}
