fc5d90682ba36c74d2deb617a7634c32ff4cf50e
Roger Torrent <78926335+R-Torrent@users.noreply.github.com>
2025-08-29 19:48:31 +0200

    functions to check potential golf strokes

--- Puzzles/ClassicPuzzle/Hard/WinamaxSponsoredContest.cpp
+++ Puzzles/ClassicPuzzle/Hard/WinamaxSponsoredContest.cpp
@@ -1,4 +1,4 @@
-#include <ctype>
+#include <cctype>
 #include <iostream>
 
 /*
@@ -23,22 +23,70 @@ struct Candidate {
 int width;
 int height;
 
-bool Candidate *viableCandidate(const char GolfCourse[][1001], Candidate *c)
+bool checkLeft(char golfCourse[][1001], int row, int col, int col1)
+{
+	if (col1 < 0)
+		return false;
+
+	char ch;
+	for (int c = col; c >= col1; c--)
+        if ((ch = golfCourse[row][c]) != '.' && (ch != 'X' || c == col1))
+			return false;
+	return true;
+}
+
+bool checkRight(char golfCourse[][1001], int row, int col, int col1)
+{
+	if (col1 >= width)
+		return false;
+
+	char ch;
+	for (int c = col; c <= col1; c++)
+        if ((ch = golfCourse[row][c]) != '.' && (ch != 'X' || c == col1))
+			return false;
+	return true;
+}
+
+bool checkUp(char golfCourse[][1001], int row, int col, int row1)
+{
+	if (row1 < 0)
+		return false;
+
+	char ch;
+	for (int r = row; r >= row1; r--)
+        if ((ch = golfCourse[r][col]) != '.' && (ch != 'X' || r == row1))
+			return false;
+	return true;
+}
+
+bool checkDown(char golfCourse[][1001], int row, int col, int row1)
+{
+	if (row1 >= height)
+		return false;
+
+	char ch;
+	for (int r = row; r <= row1; r++)
+        if ((ch = golfCourse[r][col]) != '.' && (ch != 'X' || r == row1))
+			return false;
+	return true;
+}
+
+bool viableCandidate(char golfCourse[][1001], Candidate *c)
 {
 	for (int i = 0; i < height; i++)
 		for (int j = 0; j < width; j++) {
-			char ch = GolfCourse[i][j];
-			if (!isdigit(ch))
+			char count = golfCourse[i][j];
+			if (!isdigit(count))
 				continue;
-			ch -= '0';
-			if (checkLeft(i, j, ch)
-				*c = Candidate(i, j, i - ch, j);
-			else if (checkRight(i, j, ch)
-				*c = Candidate(i, j, i + ch, j);
-			else if (checkUp(i, j, ch);
-				*c = Candidate(i, j, i, j - ch);
-			else if (checkDown(i, j, ch);
-				*c = Candidate(i, j, i, j + ch);
+			count -= '0';
+			if (checkLeft(golfCourse, i, j, count))
+				*c = Candidate(i, j, i - count, j);
+			else if (checkRight(golfCourse, i, j, count))
+				*c = Candidate(i, j, i + count, j);
+			else if (checkUp(golfCourse, i, j, count))
+				*c = Candidate(i, j, i, j - count);
+			else if (checkDown(golfCourse, i, j, count))
+				*c = Candidate(i, j, i, j + count);
 			else
 				continue;
 			return true;
@@ -49,7 +97,7 @@ bool Candidate *viableCandidate(const char GolfCourse[][1001], Candidate *c)
 
 int main()
 {
-	char golfCourse[1001][1001];
+	char golfCourse[1000][1001];
 
 	cin >> width >> height; cin.ignore();
 	for (int i = 0; i < height; i++) {
