50dd71572c1d7fcd13bf74ee90face27b56a44f3
Roger Torrent <78926335+R-Torrent@users.noreply.github.com>
2025-08-31 03:20:18 +0200

    First candidate

--- Puzzles/ClassicPuzzle/Hard/WinamaxSponsoredContest.cpp
+++ Puzzles/ClassicPuzzle/Hard/WinamaxSponsoredContest.cpp
@@ -13,86 +13,127 @@ using namespace std;
  * the standard input according to the problem statement.
  **/
 
-struct Candidate {
-	pair<int, int> from;
-	pair<int, int> to;
-
-	Candidate(int a, int b, int c, int d): from(pair(a, b)), to(pair(b, c)) { }
-};
-
 int width;
 int height;
 
-bool checkLeft(char golfCourse[][1001], int row, int col, int col1)
+void duplicateCourse(char (&copy)[1000][1001], const char (&original)[1000][1001])
 {
+	for (int i = 0; i < height; i++)
+		for (int j = 0; j <= width; j++)
+			copy[i][j] = original[i][j];
+}
+
+void backtrack(char (&)[1000][1001]);
+
+void hitLeft(char (&golfCourse)[1000][1001], const int row, const int col, int count)
+{
+	const int col1 = col - count;
 	if (col1 < 0)
-		return false;
+		return;
 
-	char ch;
-	for (int c = col; c >= col1; c--)
-        if ((ch = golfCourse[row][c]) != '.' && (ch != 'X' || c == col1))
-			return false;
-	return true;
+	int c;
+	for (c = col - 1; c >= col1; c--) {
+        const char ch = golfCourse[row][c];
+        if (!(ch == '.' || (ch == 'X' && c != col1) || (ch == 'H')))
+			return;
+	}
+	char next[1000][1001];
+	duplicateCourse(next, golfCourse);
+	for (c = col; c > col1 && next[row][c] != 'H'; c--)
+		next[row][c] = '<';
+	if (next[row][c] == 'H')
+		count = 1;
+	next[row][c] = '0' + count - 1;
+	backtrack(next);
 }
 
-bool checkRight(char golfCourse[][1001], int row, int col, int col1)
+void hitRight(char (&golfCourse)[1000][1001], const int row, const int col, int count)
 {
+	const int col1 = col + count;
 	if (col1 >= width)
-		return false;
+		return;
 
-	char ch;
-	for (int c = col; c <= col1; c++)
-        if ((ch = golfCourse[row][c]) != '.' && (ch != 'X' || c == col1))
-			return false;
-	return true;
+	int c;
+	for (c = col + 1; c <= col1; c++) {
+		const char ch = golfCourse[row][c];
+        if (!(ch == '.' || (ch == 'X' && c != col1) || (ch == 'H')))
+			return;
+	}
+	char next[1000][1001];
+	duplicateCourse(next, golfCourse);
+	for (c = col; c < col1 && next[row][c] != 'H'; c++)
+		next[row][c] = '>';
+	if (next[row][c] == 'H')
+		count = 1;
+	next[row][c] = '0' + count - 1;
+	backtrack(next);
 }
 
-bool checkUp(char golfCourse[][1001], int row, int col, int row1)
+void hitUp(char (&golfCourse)[1000][1001], const int row, const int col, int count)
 {
+	const int row1 = row - count;
 	if (row1 < 0)
-		return false;
+		return;
 
-	char ch;
-	for (int r = row; r >= row1; r--)
-        if ((ch = golfCourse[r][col]) != '.' && (ch != 'X' || r == row1))
-			return false;
-	return true;
+	int r;
+	for (r = row - 1; r >= row1; r--) {
+    	const char ch = golfCourse[r][col];
+    	if (!(ch == '.' || (ch == 'X' && r != row1) || (ch == 'H')))
+			return;
+	}
+	char next[1000][1001];
+	duplicateCourse(next, golfCourse);
+	for (r = row; r > row1 && next[r][col] != 'H'; r--)
+		next[r][col] = '^';
+	if (next[r][col] == 'H')
+		count = 1;
+	next[r][col] = '0' + count - 1;
+	backtrack(next);
 }
 
-bool checkDown(char golfCourse[][1001], int row, int col, int row1)
+void hitDown(char (&golfCourse)[1000][1001], const int row, const int col, int count)
 {
+	const int row1 = row + count;
 	if (row1 >= height)
-		return false;
+		return;
 
-	char ch;
-	for (int r = row; r <= row1; r++)
-        if ((ch = golfCourse[r][col]) != '.' && (ch != 'X' || r == row1))
-			return false;
-	return true;
+	int r;    
+	for (r = row + 1; r <= row1; r++) {
+		const char ch = golfCourse[r][col];
+    	if (!(ch == '.' || (ch == 'X' && r != row1) || (ch == 'H')))
+			return;
+	}
+	char next[1000][1001];
+	duplicateCourse(next, golfCourse);
+	for (r = row; r < row1 && next[r][col] != 'H'; r++)
+		next[r][col] = 'v';
+	if (next[r][col] == 'H')
+		count = 1;
+	next[r][col] = '0' + count - 1;
+	backtrack(next);
 }
 
-bool viableCandidate(char golfCourse[][1001], Candidate *c)
+// https://en.wikipedia.org/wiki/Backtracking
+void backtrack(char (&candidate)[1000][1001])
 {
 	for (int i = 0; i < height; i++)
 		for (int j = 0; j < width; j++) {
-			char count = golfCourse[i][j];
-			if (!isdigit(count))
-				continue;
-			count -= '0';
-			if (checkLeft(golfCourse, i, j, count))
-				*c = Candidate(i, j, i - count, j);
-			else if (checkRight(golfCourse, i, j, count))
-				*c = Candidate(i, j, i + count, j);
-			else if (checkUp(golfCourse, i, j, count))
-				*c = Candidate(i, j, i, j - count);
-			else if (checkDown(golfCourse, i, j, count))
-				*c = Candidate(i, j, i, j + count);
-			else
-				continue;
-			return true;
+			char count = candidate[i][j];
+			if (isdigit(count) && count != '0') {
+				count -= '0';
+				hitLeft(candidate, i, j, count);
+				hitRight(candidate, i, j, count);
+				hitUp(candidate, i, j, count);
+				hitDown(candidate, i, j, count);
+				return;
+			}
 		}
-
-	return false;
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++)
+			if (candidate[i][j] == '0')
+				candidate[i][j] = '.';
+		cout << candidate[i] << endl;
+	}
 }
 
 int main()
@@ -103,4 +144,6 @@ int main()
 	for (int i = 0; i < height; i++) {
 		cin >> golfCourse[i]; cin.ignore();
 	}
+
+	backtrack(golfCourse);
 }
