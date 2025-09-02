04b84904720b5af32ff3bfae8cdd4e5d7a1a8061
Roger Torrent <78926335+R-Torrent@users.noreply.github.com>
2025-09-02 19:28:59 +0200

    Serialized Grid's map

--- Puzzles/ClassicPuzzle/Hard/VoxCodeiEpisode1.cpp
+++ Puzzles/ClassicPuzzle/Hard/VoxCodeiEpisode1.cpp
@@ -1,6 +1,9 @@
 #include <algorithm>
 #include <iostream>
+#include <map>
+#include <queue>
 #include <string>
+#include <utility>
 #include <vector>
 
 using namespace std;
@@ -13,16 +16,57 @@ using namespace std;
 #define MAXHEIGHT 20
 
 struct Grid {
-	int gameTurn; // number of bombs left
-	int bombs;
+	int gameTurn;
+	int bombs; // number of bombs left
 	string mapRow[MAXHEIGHT];
-	vector<reference_wrapper<Grid>> neighbors;
+	vector<reference_wrapper<Grid>> children;
 	bool discovered; // see DFS below
 
 	Grid(): gameTurn(0), discovered(false) { }
 
+	void spawnChildren()
+	{
+		Grid nextTurn(*this);
+		nextTurn.gameTurn--;
+		queue<pair<int, int>> explosions;
+		for (int i = 0; i < height; i++)
+			for (int j = 0; j < width; j++)
+				if (mapRow[i][j] == 'P') // fork-bomb placed
+					nextTurn.mapRow[i][j] = 'C';  // fork-bomb charging
+				else if (mapRow[i][j] == 'C')
+					explosions.push(pair(i, j));
+		for (int i = 0; i < height; i++)
+			for (int j = 0; j < width; j++)
+				if (nodesHit(i, j)) {
+					Grid child(nextTurn);
+					child.mapRow[i][j] = 'P';
+					child.bombs--;
+					children.push_back(*grids.find(child.add()));
+				}
+	}
+
+	string serialize() const
+	{
+		string temp = "";
+		for (const auto& s : mapRow) {
+			int len = s.length();
+			temp += to_string(len) + "~" + s;
+		}
+
+		return temp;
+	}
+
+	string add() const
+	{
+		const string str = serialize();
+		grids[str] = *this;
+
+		return str;
+	}
+
 	static int width; // width of the firewall grid
 	static int height; // height of the firewall grid
+	static map<string, Grid> grids; // collection of all grids
 };
 
 // https://en.wikipedia.org/wiki/Depth-first_search
@@ -30,7 +74,7 @@ struct Grid {
 void DFS(Grid& grid)
 {
 	grid.discovered = true;
-	for (Grid& neighbor : grid.neighbors)
+	for (Grid& neighbor : grid.children)
 		if (!neighbor.discovered)
 			DFS(neighbor);
 }
@@ -44,6 +88,7 @@ int main()
 
 	int rounds; // number of rounds left before the end of the game
 	cin >> rounds >> grid0.bombs; cin.ignore();
+	grid0.add();
 
 	// game loop
 	while (true) {
